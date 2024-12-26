feee039d5bbad54430a4ffa381637a9c7f3fd425
barel1795 <barel.ch.17@gmail.com>
2024-12-26 13:28:22 +0200

    gfyu

---
+++ twist/task8/script.js
@@ -0,0 +1,9 @@
+function load(){
+	console.log("loaded");
+}
+
+function divClicked(){
+	document.getElementById("dorothy").style.left = "300px";
+	document.getElementById("dorothy").style.top = "400px";
+	console.log("clicked");
+}
